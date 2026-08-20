9cc76fb470f90f15dda86212975290cbde472ac8
“nguyendinhduong301” <nguyendinhduong301@gmail.com>
2026-08-20 15:06:40 +0700

    UpdateCode

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164a68f88f0cd541acabb50e2405955094b11294")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
